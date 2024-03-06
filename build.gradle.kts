38fdf955772a122fde0672080391a67e2a8e6e28
filberol <mkh.vadim@gmail.com>
2024-03-06 12:07:35 +0300

    Refactor build script, add report

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,10 @@ plugins {
     kotlin("jvm") version "1.9.22"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
-    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
-
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation ("org.junit.jupiter:junit-jupiter-params:5.1.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(17)
 }
